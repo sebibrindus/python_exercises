93fa15265bd3db4b876640e72c6814115d5b22a1
Basty <sebibrindus@gmail.com>
2022-05-16 12:08:53 +0300

    New factorial function

---
+++ factorial.py
@@ -0,0 +1,18 @@
+def factorial(max_range):
+    '''
+    A function that multiplies every number between 1 and a given max number
+    :param max_range: the maximum range for the factorial
+    :return: returns the total product of the numbers
+    '''
+    product = 1
+    for i in range(1, max_range + 1, 1):
+        product *= i
+    return product
+
+
+max_factorial = int(input("Please enter a max number: "))
+
+for i in range(1, max_factorial + 1, 1):
+    print(str(i) + ": " + str(factorial(i)))
+# the loop above prints all factorials between 1 and the given number
+
